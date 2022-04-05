10871c7f6b67d26c96fc57034d0bacac3a2e5669
Eleanor Quandt <elquandt@gmail.com>
2022-04-04 20:00:38 -0700

    add list of faves

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-const beverage = $("#beverage").val(); 
+const superhero = $("#superhero").val(); 
+const genre = $("input:radio[name=genre]:checked").val();
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const nameInput = $("input#name").val();
+    const favFoodInput = $("input#favFood").val();
+    const favAnimalInput= $("input#favanimal").val();
+  });
+});
